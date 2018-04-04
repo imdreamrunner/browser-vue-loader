85dd9402692b4444e55720195371e9b916de9760
Ivor Zhou <ivorz@sg.ibm.com>
2018-04-04 14:26:50 +0800

    Initial commit.

---
+++ app/index.js
@@ -0,0 +1,10 @@
+/**
+ * Application entry point
+ */
+
+// Load application styles
+import 'styles/index.scss';
+
+// ================================
+// START YOUR APP HERE
+// ================================
