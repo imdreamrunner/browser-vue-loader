f9d3477ed76457e3e9fd1d58ca156544a2f812b9
Ivor Zhou <ivorz@sg.ibm.com>
2018-04-06 22:07:04 +0800

    Reorganize demo.

--- test-assets/sample-es-module.js
+++ test-assets/sample-es-module.js
@@ -1,6 +1,5 @@
 import another from './another-module.js';
 import _ from 'amd!https://unpkg.com/lodash@4.17.5/lodash.js';
-import Vue from 'commonjs!https://cdn.jsdelivr.net/npm/vue@2.5.16/dist/vue.js';
 
 export function currentModuleFunction() {
   return 1;
