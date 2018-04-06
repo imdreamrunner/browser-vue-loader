4f254747f10efd9563f0e4899ef8b0aa9b1c68e6
Ivor Zhou <imdreamrunner@gmail.com>
2018-04-07 02:42:33 +0800

    Add scopeId and functional template to vue sfc.

--- src/processors/vue-sfc/index.js
+++ src/processors/vue-sfc/index.js
@@ -1,5 +1,6 @@
 import { ModuleNamespace } from 'es-module-loader/core/loader-polyfill'
 import { parseComponent } from 'vue-template-compiler'
+import md5 from 'md5'
 import * as componentNormalizer from './component-normalizer'
 import BaseProcessor from '../base-processor'
 import componentTemplate from 'raw-loader!./component-template.txt'
@@ -16,31 +17,32 @@ export default class VueProcessor extends BaseProcessor {
     // register component-normalizer if it's not inside the registry.
     if (!this.loader.registry.has('component-normalizer')) {
       const componentNormalizerModule = new ModuleNamespace(componentNormalizer)
-      this.loader.registry.set('component-normalizer', componentNormalizerModule)
+      await this.loader.registry.set('component-normalizer', componentNormalizerModule)
     }
 
     // <script>
     const script = parts.script.content
     const scriptKey = key + '#script'
-
     await this.loader.router.routeTo('js', scriptKey, script)
-
     transformedSource = transformedSource.replace('__vue_script__', scriptKey)
 
     // <template>
-    const templateKey = key + '#template'
-    await this.loader.router.routeTo('vue-template', templateKey, parts.template.content)
-    transformedSource = transformedSource.replace('__vue_template__', templateKey)
+    if (parts.template) {
+      const templateKey = key + '#template'
+      await this.loader.router.routeTo('vue-template', templateKey, parts.template.content)
+      transformedSource = transformedSource.replace('__vue_template__', templateKey)
 
-    // <template>
+      const functionalTemplate = parts.template.attrs && parts.template.attrs.functional || false
+      transformedSource = transformedSource.replace('__vue_template_functional__', JSON.stringify(functionalTemplate))
+    }
+
+    // <style>
     const styleKey = key + '#style'
     await this.loader.router.routeTo('css', styleKey, parts.styles[0].content)
     transformedSource = transformedSource.replace('__vue_styles__', styleKey)
 
-    await this.loader.registry.set('empty', new ModuleNamespace({}))
-    transformedSource = transformedSource.replace('__vue_template_functional__', 'empty')
-    transformedSource = transformedSource.replace('__vue_scopeId__', 'empty')
-    transformedSource = transformedSource.replace('__vue_module_identifier__', 'empty')
+    const moduleId = 'data-v-' + md5(key)
+    transformedSource = transformedSource.replace('__vue_scopeId__', moduleId)
 
     await this.loader.router.routeTo('js', key, transformedSource)
   }
