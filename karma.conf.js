9bdd719ef76a5aa66e57ac56638b918edfccd980
Ivor Zhou <imdreamrunner@gmail.com>
2018-04-09 23:00:29 +0800

    Add karma.

---
+++ karma.conf.js
@@ -0,0 +1,43 @@
+module.exports = (config) => {
+  config.set({
+
+    files: [
+      { pattern: 'dist/*.js', watched: true, served: true, included: false },
+      { pattern: 'examples/**/*', watched: true, served: true, included: false },
+      { pattern: 'tests/**/*-test.js', watched: true },
+    ],
+
+    frameworks: ['browserify', 'mocha'],
+
+    preprocessors: {
+      'tests/**/*-test.js': ['browserify'],
+    },
+
+    browserify: {
+      debug: true,
+      transform: [
+        ['babelify', {
+          ignore: /node_modules/
+        }],
+      ],
+      extensions: ['.jsx']
+    },
+
+    babelPreprocessor: {
+      options: {
+        sourceMap: 'inline'
+      },
+      sourceFileName: function(file) {
+        return file.originalPath;
+      }
+    },
+
+    plugins: [
+      require('karma-mocha'),
+      require('karma-browserify'),
+      require('karma-chrome-launcher'),
+    ],
+
+    browsers: ['Chrome'],
+  })
+}
---
+++ tests/e2e/loadvue-test.js
@@ -0,0 +1,8 @@
+import { describe, it } from 'mocha'
+import { expect } from 'chai'
+
+describe('hexColorLuminance', function () {
+  it('should return a luminanced color', function () {
+    expect({a:1}).is.not.empty
+  })
+})
